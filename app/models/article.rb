41f3f9d8f505323a80e6a82fc6290de0df2a5db9
Laud Bentil <laud.bentil@meltwater.org>
2014-11-19 21:48:12 +0000

    added rss parsing & 'title' field to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,18 @@
 class Article < ActiveRecord::Base
-  
+   def fetch
+    require 'open-uri'
+    require 'simple-rss'
+    
+  # save rss data from website into 'rss'
+    url = 'http://feeds.theguardian.com/theguardian/football/rss'
+    rss = SimpleRSS.parse open(url)
+
+  #iterate through feed save title into variable 
+    rss.items.each do |r|
+      t = r.title
+      t.to_s
+      @article = Article.create(title: t)
+    end 
+
+  end
 end
