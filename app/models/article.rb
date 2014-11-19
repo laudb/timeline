8d30be5cd76c79d780b76f5b72413e85c527c837
Laud Bentil <laud.bentil@meltwater.org>
2014-11-19 22:15:17 +0000

    added 'link','description' & 'UTF-8' encoding

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,17 +1,26 @@
 class Article < ActiveRecord::Base
-   def fetch
+  
+  def fetch
+  
     require 'open-uri'
     require 'simple-rss'
-    
+
   # save rss data from website into 'rss'
     url = 'http://feeds.theguardian.com/theguardian/football/rss'
     rss = SimpleRSS.parse open(url)
 
   #iterate through feed save title into variable 
     rss.items.each do |r|
+
       t = r.title
-      t.to_s
-      @article = Article.create(title: t)
+      t.force_encoding("UTF-8")
+      l =r.link
+      l.force_encoding("UTF-8")
+      d = r.description
+      d.force_encoding("UTF-8")
+
+      @article = Article.create(title: t, link: l, description: d)
+  
     end 
 
   end
