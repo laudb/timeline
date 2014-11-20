3613648b6b2b23ccfdf60796fa9e5b72f5f32e00
Laud Bentil <laud.bentil@meltwater.org>
2014-11-20 17:03:44 +0000

    added extra url to article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,20 +5,26 @@ class Article < ActiveRecord::Base
     require 'open-uri'
     require 'simple-rss'
 
-  # save rss data from website into 'rss'
-    url = 'http://feeds.theguardian.com/theguardian/football/rss'
-    rss = SimpleRSS.parse open(url)
+    #array contains rss links
+    url = ['http://feeds.theguardian.com/theguardian/football/rss',
+           'http://sports.yahoo.com/soccer//rss.xml',
+           'http://sports.espn.go.com/espn/rss/news'
+          ]
+      
+    #loop through each link
+    url.each do
+      # save rss data from website into 'rss'
+      rss = SimpleRSS.parse open(url)
 
-  #iterate through feed. Save title, link, description into variable 
-    rss.items.each do |r|
+        # iterate through feed. Save title, link, description
+        rss.items.each do |r|
 
-      t = r.title.force_encoding("UTF-8")
-      l = r.link.force_encoding("UTF-8")
-      d = r.description.force_encoding("UTF-8")
+          t = r.title.force_encoding("UTF-8")
+          l = r.link.force_encoding("UTF-8")
+          d = r.description.force_encoding("UTF-8")
 
-      @article = Article.create(title: t, link: l, description: d)
-  
-    end 
-
+            @article = Article.create(title: t, link: l, description: d)
+        end 
+    end
   end
 end
